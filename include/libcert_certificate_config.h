c22f7985e63b70758f6e191ab7aed75fe2d7dc6f
Mauro Guerrera <mauroguerrera@eaton.com>
2020-01-27 17:20:20 +0100

    Update license headers

--- include/libcert_certificate_config.h
+++ include/libcert_certificate_config.h
@@ -1,7 +1,7 @@
 /*  =========================================================================
     libcert_certificate_config - X509 Certificate configuration
 
-    Copyright (C) 2014 - 2019 Eaton
+    Copyright (C) 2014 - 2020 Eaton
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
